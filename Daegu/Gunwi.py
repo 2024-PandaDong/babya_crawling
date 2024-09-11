0258ab67bbe7fbb6550b344f791b2f4cec5697c1
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-09-12 08:32:07 +0900

    feat :: 대구 군위군 정책 크롤링 완성

--- Daegu/Gunwi.py
+++ Daegu/Gunwi.py
@@ -14,7 +14,7 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
 babya_server = "http://동바오.site"
-region = "104020"
+region = "104090"
 link_list = list()
 current_list = list()
 result_data = []
@@ -22,45 +22,36 @@ result_data = []
 site_url = requests.get(f"{babya_server}/policy/site", params={"region": region})
 response_data = site_url.json()
 base_url = response_data["data"]["policySiteUrl"]
+format_url = base_url.split("/main")[0]
 
 collected_site_data = requests.get(f"{babya_server}/policy/catalog", params={"site": base_url})
 old_list = [item["pageId"] for item in collected_site_data.json()["data"]]
 
-url_data = [f"{base_url}?menu_id=30001042", f"{base_url}?menu_id=00004201"]
+url = f"{format_url}/page.do?mnu_uid=448"
+driver.get(url)
+time.sleep(2)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-for url in url_data:
-    driver.get(url)
-    time.sleep(2)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    
-    for i in soup.select("ul.snb > li.open > ul > li > a"):
-        id_item = i.get("href").split("menu_id=")[1]
+for i in soup.select("ul.snb_depth2 > li > a.on"):
+    parent_div = i.find_parent('li')
+    for j in parent_div.select("li > ul > li > a"):
+        id_item = j.get("href").split("mnu_uid=")[1]
         link_list.append(id_item)
         
 for link in link_list:
-    driver.get(f"{base_url}?menu_id={link}")
+    driver.get(f"{format_url}/page.do?mnu_uid={link}")
     time.sleep(2)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    elements = soup.select("#content > div.cont_body > div.lnb")
     
-    if elements:
-        for element in elements:
-            a_remove = element.find_all('a', target='_blank')
-            for a_tag in a_remove:
-                a_tag.extract()
-            
-            for i in element.select("ul > li > a"):
-                id_item = i.get("href").split("menu_id=")[1]
-                current_list.append(id_item)
-                
-    else:
-        current_list.append(link)
-            
-            
+    for i in soup.select("#content > div.sub_tab > ul > li > a"):
+        id_item = i.get("href").split("mnu_uid=")[1]
+        current_list.append(id_item)
+
+
 page_list = set(current_list) - set(old_list)
 
 for page_id in page_list:
-    page_url = f"{base_url}?menu_id={page_id}"
+    page_url = f"{format_url}/page.do?mnu_uid={page_id}"
     driver.get(page_url)
     time.sleep(2)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -73,24 +64,19 @@ for page_id in page_list:
         "site": None
     }
     
-    delete_element = soup.find('div', class_="lnb")
-    
-    # HTML 코드 주석 삭제
     for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
         comment.extract()
+    
+    for title in soup.select("div.sub_right_top > h3"):
+        data_dict["title"] = title.get_text()
         
-    if delete_element:
-        for title in soup.select("#content > div.cont_body > div.lnb > ul > li.on > a"):
-            data_dict["title"] = title.get_text()
-            
-    else:
-        for title in soup.select("#content > header.cont_head > h1.title"):
-            data_dict["title"] = ' '.join(title.get_text().split())
+    for edit_date in soup.select("div.research_box > div.title > ul.dataOffer > li.update"):
+        data_dict["editDate"] = edit_date.get_text().split(": ")[1].strip()
+    
+    for content in soup.select("#content"):
+        for tag in content.find_all(['h4', 'div', 'div'], class_=['hidden', 'sub_tab', 'research_box']):
+            tag.extract()
         
-    for content in soup.select("#content > div.cont_body"):
-        if delete_element:
-            delete_element.extract() # div 태그의 class명이 lnb인 태그 제거
-            
         for img in content.find_all('img'):
             img_url = img.get("src")
             img["src"] = urllib.parse.urljoin(base_url, img_url)
@@ -100,9 +86,6 @@ for page_id in page_list:
             a['href'] = urllib.parse.urljoin(base_url, file_url)
             
         data_dict["content"] = re.sub(r'[\s\u00A0-\u00FF]+', " ", str(content).replace('"', "'"))
-        
-    for edit_date in soup.select("#content > footer.cont_foot > div.cont_manager > dl.update > dd"):
-        data_dict["editDate"] = ' '.join(edit_date.get_text().split()).replace(".", "-")
         
     data_dict["pageId"] = page_id
     data_dict["site"] = base_url
@@ -118,7 +101,7 @@ if (len(result_data) > 0):
     
 else:
     print("아직 새로운 정책이 업데이트 되지 않았습니다.")
-
+        
 driver.close()
 sys.exit()
 
